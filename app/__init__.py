af68369912f5474c42703cc1a2f684eb1b6856d6
Sheetal Umesh Kumar <sheetaluk@gmail.com>
2015-01-26 02:12:37 -0500

    Actually fixing cors issue. I think.

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,20 @@ import os
 import riak
 from flask import Flask
 from datastore import Datastore
-from flask.ext.cors import CORS, cross_origin
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
 
 cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
 
 app.config['radius'] = 1 #km
 
 app.config['riak_node'] = '54.164.165.43'
 app.config['riak_http_port'] = 8098
 app.config['riak_protocol'] = 'http'
-app.config['riak_foodcarts_bucket'] = 'foodcarts_test4'
+app.config['riak_foodcarts_bucket'] = 'foodcarts_test5'
 app.config['riak_foodcarts_for_geohash_bucket'] = \
-  'foodcarts_for_geohash_test7'
+  'foodcarts_for_geohash_test10'
 
 riak_client = riak.RiakClient(
   protocol=app.config['riak_protocol'], \
--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@ import json
 
 from app import app
 from flask import request
+from flask.ext.cors import cross_origin
 
 import foodcarts
 
@@ -14,6 +15,7 @@ Param user_long string longitude of user.
 Returns Json list of foodcart objects.
 """
 @app.route('/api/v1.0/nearest_foodcarts', methods=['GET'])
+@cross_origin(origins="*")
 def get_nearest_foodcarts():
   lat = float(request.args.get('user_lat'))
   long = float(request.args.get('user_long'))
